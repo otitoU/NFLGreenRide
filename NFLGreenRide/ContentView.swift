f371431e37e907b0bdbdb29ad3edca6c8c063372
OtitoU <136706167+Odera19@users.noreply.github.com>
2024-02-24 16:14:33 -0600

    Initial Commit

---
+++ NFLGreenRide/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NFLGreenRide
+//
+//  Created by Otito Udedibor on 2/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NFLGreenRide/NFLGreenRideApp.swift
@@ -0,0 +1,17 @@
+//
+//  NFLGreenRideApp.swift
+//  NFLGreenRide
+//
+//  Created by Otito Udedibor on 2/24/24.
+//
+
+import SwiftUI
+
+@main
+struct NFLGreenRideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
